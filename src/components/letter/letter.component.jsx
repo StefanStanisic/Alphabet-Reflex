41966a182e9f9bb48e8d39e23feaa34613e1adf5
Stefan Stanisic <stefan77403@gmail.com>
2021-07-16 15:14:37 +0200

    Added game logic and some utils. Now to add reset game.

--- src/components/letter/letter.component.jsx
+++ src/components/letter/letter.component.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './letter.styles.css'
 
-const LetterDisplay = ({letter, number, color}) => {
+const LetterDisplay = ({letter, number, state}) => {
+
+  const objState = state.filter(elem => elem.letter === letter)[0]
+
+ function status() {
+   if(objState) return {color: objState.inTime && objState.correct ? "green" : "red"}
+   if(!objState) return {color: "gray"}
+ }
+
   return (
-    <p className='values' style={{color: color}}>{letter}  ({number})  </p>
+    <p className='values' style={status()}>{letter} ({number})</p>
   )
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,74 +1,123 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import LetterDisplay from '../../components/letter/letter.component';
-// import ScoreDisplay from '../../components/score/score.component';
+import ScoreDisplay from '../../components/score/score.component';
 
 import letters from '../../data/letters';
+import numbers from '../../data/numbers';
+import { cl } from '../../utils';
 
 import './homepage.styles.css';
 
 const HomePage = () => {
-  const [gameStarted, setGameStarted] = useState('false');
+  const [gameStarted, setGameStarted] = useState(false);
   const [gameDifficulty, setGameDifficulty] = useState(0);
+  const [displayNumber, setDisplayNumber] = useState();
   const [lettersHit, setLettersHit] = useState(0);
+  const [timerExpired, setTimerExpired] = useState(false)
   const [lettersMissed, setLettersMissed] = useState(0);
+
+  const [inputState, setInputState] = useState([])
+
   const radioRef1 = useRef(null);
   const radioRef2 = useRef(null);
   const radioRef3 = useRef(null);
+  let numbersMap = numbers;
 
   const startGame = () => {
-    if(gameDifficulty !== 0) {
-      setGameStarted(!gameStarted); 
+    if (gameDifficulty !== 0) {
+      setGameStarted(true);
       radioRef1.current.disabled = true;
       radioRef2.current.disabled = true;
       radioRef3.current.disabled = true;
+      const randomNumber = numbersMap.get([...numbersMap.keys()][Math.floor(Math.random() * numbersMap.size)]);
+      setDisplayNumber(randomNumber);
     }
   }
 
+  const inputHandler = (e) => {
+    const inputString = e.target.value.slice(e.target.value.length - 1, e.target.value.length)
+    const shouldBe = letters.filter(elem => elem.number === displayNumber)[0].letter
+    if (shouldBe === inputString) {
+      setInputState(prev => [...prev, { letter: inputString, inTime: timerExpired, correct: shouldBe === inputString }])
+    } else {
+      setInputState(prev => [...prev, { letter: shouldBe, inTime: false, correct: false }])
+    }
+  }
+
+  useEffect(() => {
+    if (gameStarted) {
+      setTimerExpired(true)
+      setTimeout(() => {
+        setTimerExpired(false)
+        startGame()
+      }, gameDifficulty);
+    }
+  }, [displayNumber])
+
+  useEffect(() => {
+
+    if (displayNumber && !timerExpired) {
+      const shouldBe = letters.filter(elem => elem.number === displayNumber)[0].letter
+      const inState = inputState.filter(elem => elem.letter === shouldBe)
+
+      if (shouldBe !== inState[0]?.letter) {
+        setInputState(prev => [...prev, { letter: shouldBe, inTime: false, correct: false }])
+      }
+    }
+
+  }, [timerExpired])
+
+
   const stopGame = () => {
-    setGameStarted(!gameStarted);
+    setGameStarted(!false);
     radioRef1.current.disabled = false;
     radioRef2.current.disabled = false;
     radioRef3.current.disabled = false;
   }
 
-  let lettersArray = [];
-  letters.forEach(elem => {
-    lettersArray.push(<LetterDisplay letter={elem.letter} number={elem.number} key={elem.number}/>)
-  })
-
   return (
     <div className='main-container'>
       <div className='difficulty-container'>
-        <form className='difficulty' id="form"> 
-          <input type="radio" name="difficulty" value="Easy" onClick={() => {setGameDifficulty(5000)}}  ref={radioRef1}/>
-          <label>Easy</label>
-          <input type="radio" name="difficulty" value="Medium" onClick={() => {setGameDifficulty(3500)}} ref={radioRef2} />
-          <label>Medium</label>
-          <input type="radio" name="difficulty" value="Hard" onClick={() => {setGameDifficulty(2000)}}  ref={radioRef3}/>
-          <label>Hard</label>
+        <form className='difficulty' id="form">
+          <input type="radio" name="difficulty" value="Easy" onClick={() => { setGameDifficulty(5000) }} ref={radioRef1} />
+          <label>Easy</label>
+          <input type="radio" name="difficulty" value="Medium" onClick={() => { setGameDifficulty(3500) }} ref={radioRef2} />
+          <label>Medium</label>
+          <input type="radio" name="difficulty" value="Hard" onClick={() => { setGameDifficulty(2000) }} ref={radioRef3} />
+          <label>Hard</label>
         </form>
       </div>
 
       <div className='start-button-container'>
         {
-          gameStarted ? 
-            <button className='start-stop-button' onClick={startGame}>Start Game</button> 
-          : <button className='start-stop-button' onClick={stopGame}>Stop Game</button>
+          !gameStarted ?
+            <button className='start-stop-button' onClick={startGame}>Start Game</button>
+            : <button className='start-stop-button' onClick={stopGame}>Stop Game</button>
         }
       </div>
 
       <div className="number-display-container">
-        <p className='number-display'>15</p>
+        <p className='number-display'>{displayNumber}</p>
       </div>
 
       <div className="input-container">
-        <input type="text" name="letter-input" className='letter-input' placeholder='input-letter' />
+        <input type="text" name="letter-input" className='letter-input' placeholder='input-letter' onChange={inputHandler} />
       </div>
 
-      <div className="letters-container">
-        {lettersArray}
+      <ScoreDisplay hit={lettersHit} miss={lettersMissed} left={0}></ScoreDisplay>
+
+      <div className="letters">
+        <div className="letters-container">
+          {/* {lettersArray} */}
+          {
+            letters.map(elem => (
+              <LetterDisplay letter={elem.letter} number={elem.number} key={elem.number} state={inputState} />
+            ))
+          }
+        </div>
       </div>
+
     </div>
   );
 };
